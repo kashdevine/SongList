aa1e2a20fde45aa2afa767cef34f581c5df5491e
kash <kasheen.mowatt@gmail.com>
2020-10-14 19:39:35 -0700

    Add project files.

---
+++ SongList/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SongList.Models;
+
+namespace SongList.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private SongContext context { get; set; }
+
+        public HomeController(SongContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var songs = context.Songs.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+
+            return View(songs);
+        }
+    }
+}
